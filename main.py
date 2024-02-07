56ff759231ccd7bd10534ed608de698e1fd00522
Guilherme H <ghpm99@gmail.com>
2024-02-07 15:12:02 -0300

    Feat: implements feature 4

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from feature1 import feature1
 
-from feature2 import feature2
+from feature2 import feature2, nome
 
 from feature3 import feature3
 
@@ -10,6 +10,7 @@ def base():
     feature1()
     feature2()
     feature3()
+    print(nome)
 
 
 if __name__ == '__main__':
